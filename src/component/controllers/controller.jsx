fffabc4a7716f57a651d1cb4490dec42a677235f
ahmed Khalifa <ahmedtsjust94@gmail.com>
2025-04-27 19:36:23 +0300

    folder added

---
+++ src/component/controllers/controller.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function controller() {
+  return <div>controller</div>;
+}
---
+++ src/component/viedo/viedo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function viedo() {
+  return <div>viedo</div>;
+}
